c1f079261113be94e6eb664bb6201b7ba8f05849
Thanh Le <nhatthanhh1993@gmail.com>
2019-02-01 11:52:22 +0200

    validate Inputs

--- src/pages/login-register/login-register.ts
+++ src/pages/login-register/login-register.ts
@@ -6,6 +6,7 @@ import { Http, Headers } from "@angular/http";
 import { HttpClient } from "@angular/common/http";
 import { User, LoginResponse, UsernameResponse } from "../../interfaces/pic2";
 import { MediaProvider } from "../../providers/media/media.provider";
+import { NgForm } from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -15,8 +16,10 @@ import { MediaProvider } from "../../providers/media/media.provider";
 export class LoginRegisterPage {
   user: User = { username: null };
   onRegister = false;
-  //   username = "";
-  //   password = "";
+  usernameError = "";
+  passwordError = "";
+  emailError = "";
+
   constructor(
     public mediaprovider: MediaProvider,
     public navCtrl: NavController,
@@ -31,7 +34,8 @@ export class LoginRegisterPage {
       this.navCtrl.push(HomePage);
     }
   }
-  loginClicked(form) {
+  loginClicked(formSignIn) {
+    // console.log(formSignIn);
     // this.mediaprovider.login(this.user).subscribe(
     //   (response: LoginResponse) => {
     //     console.log("response");
@@ -47,66 +51,83 @@ export class LoginRegisterPage {
     //     console.log(error);
     //   }
     // );
-    console.log(form);
   }
   //thanhvl@metropolia.fi
 
-  registerClicked(form) {
-    console.log(form);
+  registerClicked(formSignUp) {
+    console.log(formSignUp);
 
-    // this.mediaprovider.checkIfUserExists(this.user).subscribe(
-    //   (response: UsernameResponse) => {
-    //     console.log(response);
-    //     if (response.available) {
-    //       this.mediaprovider.register(this.user).subscribe(
-    //         (response: LoginResponse) => {
-    //           localStorage.setItem("token", response.token);
-    //           this.navCtrl.push(HomePage);
-    //           console.log(response);
-    //         },
-    //         error => {
-    //           console.log(error);
-    //         }
-    //       );
+    // if (formSignUp.valid) {
+    //   this.mediaprovider.checkIfUserExists(this.user).subscribe(
+    //     (response: UsernameResponse) => {
+    //       console.log(response);
+    //       if (response.available) {
+    //         this.mediaprovider.register(this.user).subscribe(
+    //           (response: LoginResponse) => {
+    //             localStorage.setItem("token", response.token);
+    //             this.navCtrl.push(HomePage);
+    //             console.log(response);
+    //           },
+    //           error => {
+    //             console.log(error);
+    //           }
+    //         );
+    //       }
+    //     },
+    //     error => {
+    //       console.log(error);
     //     }
-    //   },
-    //   error => {
-    //     console.log(error);
-    //   }
-    // );
+    //   );
+    // }
   }
 
   openRegisterForm() {
     this.onRegister = true;
-    this.user.username = null;
   }
 
   checkUsername() {
+    this.usernameError = "";
     if (this.user.username != null) {
-      this.mediaprovider.checkIfUserExists(this.user).subscribe(
-        (response: UsernameResponse) => {
-          console.log(response);
-          if (!response.available) {
-            alert("this username is not available!");
+      if (this.user.username.length < 3) {
+        this.usernameError = "The username should be more than 3 characters.";
+      } else {
+        this.mediaprovider.checkIfUserExists(this.user).subscribe(
+          (response: UsernameResponse) => {
+            console.log(response);
+            if (!response.available) {
+              this.usernameError = "this username is not available!";
+            }
+          },
+          error => {
+            console.log(error);
           }
-        },
-        error => {
-          console.log(error);
-        }
-      );
+        );
+      }
     } else {
-      alert("please, enter the username");
+      this.usernameError = "please, enter the usernam";
     }
   }
 
-  confirmPassword() {
-    if (this.user.password !== this.user.password2) {
-      alert(
-        "passwords do not match.." +
-          this.user.password +
-          ":" +
-          this.user.password2
-      );
+  checkPassword() {
+    this.passwordError = "";
+
+    if (this.user.password == null) {
+      this.passwordError = "Please, enter your password";
+    } else if (this.user.password.length < 5) {
+      this.passwordError = "The username should be more than 5 characters.";
     }
+    if (this.user.password !== this.user.password2) {
+      this.passwordError += "These passwords do not match..";
+    }
+  }
+
+  checkEmail(formSignUp) {
+    this.emailError = "";
+    console.log(formSignUp.controls.email.valid);
+    if (this.user.email == null) {
+      this.emailError = "Please, enter your Email";
+    } else if (!formSignUp.controls.email.valid) {
+      this.emailError = "Email error";
+    } else this.emailError = "";
   }
 }
